eacabbc54d2dd0d7cb3830aca90ce25c5532708e
Imanol Moscoso <64240176+ImaMos01@users.noreply.github.com>
2020-06-23 17:40:42 -0500

    Visual Estudio (Original)

---
+++ Basic_Combat/Main.cpp
@@ -0,0 +1,24 @@
+#include"Combat.h"
+#include<cstdlib>
+#include<ctime>
+
+int main() {
+	srand((unsigned)time(0));
+	int const players_team = 2;
+	int const enemies_team = 3;
+
+	Player players[players_team];
+	players[0].set_values('s', 50, 100, 40, 13);//(name, hp, exp_next, damage,speed)
+	players[1].set_values('w', 40, 50, 10, 10);
+
+	Enemy enemies[enemies_team];
+	enemies[0].set_values('e', 50, 10, 50, 11); //(name,hp,damage,exp_drops,speed)
+	enemies[1].set_values('E', 50, 20, 25, 12);
+	enemies[2].set_values('b', 50, 5, 25, 9);
+
+
+	Combat enemy_appears(players, enemies, players_team, enemies_team);
+
+	return 0;
+}
+
--- Basic_Combat/player.h
+++ Basic_Combat/player.h
@@ -1,34 +1,37 @@
 #ifndef Player_H
 #define Player_H
+#include<iostream>
 
-class Player{
+class Player {
 	private:
 		char name;
 		int level;
 		int exp;
 		int expNext;
-	
+
 		int hp;
 		int hpmax;
 		int damage;
 		int speed;
-		
+
 	public:
 		Player();
-		Player(char n, int h, int expNext, int da, int spe);
+		Player(char n, int h, int Next, int da, int spe);
 		virtual ~Player();
-		
-		void set_values(char n, int h, int expNext, int da, int spe);
+
+		void set_values(char n, int h, int Next, int da, int spe);
 		int get_damage();
 		char get_name();
-		
+
 		void gain_level(const int expEnemy);
 		void update_Stats();
 		void show_stats();
 		int get_exp();
-		
+
 		void take_damage(const int damage);
 		int get_hp();
 		int get_speed();
 };
 #endif
+
+
